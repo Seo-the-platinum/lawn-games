e728713d24ba31e22f7c5710f7901a41bb5ab0b5
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-11-11 21:36:22 -0800

    added react-icons and started working on hamburger menu

--- src/app/components/Nav/Hamburger.tsx
+++ src/app/components/Nav/Hamburger.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import Link from 'next/link'
 
 const Hamburger = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(curr => !curr)
+  }
   return (
-    <div>Hamburger</div>
+    <div className={`${isOpen ? '-translate-x-1/4' : 'translate-x-0'} relative flex sm:hidden left-full transition-transform ease-in-out duration-500`}>
+      <GiHamburgerMenu className='text-4xl fill-slate-50 absolute right-full' onClick={toggle} />
+      <div className="flex flex-col min-h-screen pl-2">
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/gallery'>Gallery</Link>
+          </li>
+          <li>
+            <Link href='/booking'>Booking</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import Hamburger from './Hamburger'
 
 const Nav = () => {
   return (
-    <div className='bg-slate-950 px-4'>
+    <div className='bg-slate-950 overflow-x-hidden max-w-full'>
       <ul className='hidden gap-4 justify-end sm:flex text-2xl'>
         <li>
           <Link href='/'>
@@ -26,6 +27,7 @@ const Nav = () => {
           </Link>
         </li>
       </ul>
+      <Hamburger />
     </div>
   )
 }
